b127dd127251cedf25445bf7f9f2779468681f43
MadiSovetbekov <31875048+MadiSovetbekov@users.noreply.github.com>
2017-10-14 04:25:43 +0600

    Add week5

---
+++ week5/script2.js
@@ -0,0 +1,7 @@
+function changeImage(event) {
+	var src = event.currentTarget.src;
+	var newImg = document.querySelector("img");
+	newImg.src = src;
+	var cur = document.querySelector("bigImage");
+	document.querySelector("bigImage");
+}
